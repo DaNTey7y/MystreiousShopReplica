0bc64341f8b109cc96be65a1f5c4f9c5b5902d2a
DaNTey7y <xvhuqdph@gmail.com>
2024-02-21 22:54:01 +0500

    - Первый хендлер

--- bot/__main__.py
+++ bot/__main__.py
@@ -4,17 +4,23 @@ import logging
 from aiogram import Bot, Dispatcher
 
 from config_reader import config
+from handlers import commands
 
 
 async def main():
+    # Задаем логгирование
     logging.basicConfig(
         level=logging.INFO,
         format="%(asctime)s - %(levelname)s - %(name)s - %(message)s"
     )
 
+    # Авторизуем бота
     bot = Bot(token=config.bot_token.get_secret_value(), parse_mode="HTML")
 
     dp = Dispatcher()
+
+    # ПОдключение роутеров
+    dp.include_router(commands.router)
 
     await dp.start_polling(bot)
 
---
+++ bot/handlers/commands.py
@@ -0,0 +1,28 @@
+from aiogram import Bot, Router, F
+from aiogram.types import Message, InlineKeyboardButton
+from aiogram.filters import Command, CommandStart
+from aiogram.utils.keyboard import InlineKeyboardBuilder
+
+
+router = Router()
+router.message.filter(F.chat.type == "private")
+
+
+@router.message(CommandStart())
+async def start_cmd(message: Message):
+    await message.answer("Привет, введи /help")
+
+
+@router.message(Command("help"))
+async def help_cmd(message: Message):
+    await message.answer("🧬 Mysterious Shop 🧬\n\nКоманды:\n"
+                         "/start - Старт\n/shop - Магазин\n/help - О боте\n\n"
+                         "Разработчик:\n@xvhuqdph")
+
+
+@router.message(Command("shop"))
+async def shop_cmd(message: Message):
+    builder = InlineKeyboardBuilder()
+    builder.add(InlineKeyboardButton(
+        text="🧬 Mysterious Shop 🧬", switch_inline_query_current_chat=""))
+    await message.answer("Нажми кнопку", reply_markup=builder.as_markup())
