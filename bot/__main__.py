a7e316274512bd5309c784ec596999c4a09dbcbc
DaNTey7y <xvhuqdph@gmail.com>
2024-03-02 00:20:16 +0500

    - Подстройка для БД и завоз команд

--- bot/__main__.py
+++ bot/__main__.py
@@ -3,8 +3,12 @@ import logging
 
 from aiogram import Bot, Dispatcher
 
+from sqlalchemy.ext.asyncio import create_async_engine, async_sessionmaker
+
 from config_reader import config
+# from handlers import commands, inline_mode
 from handlers import commands
+from ui_commands import set_bot_commands
 
 
 async def main():
@@ -14,13 +18,21 @@ async def main():
         format="%(asctime)s - %(levelname)s - %(name)s - %(message)s"
     )
 
+    # Подключение к БД
+    engine = create_async_engine(url=str(config.db_url), echo=False)
+    session_maker = async_sessionmaker(engine, expire_on_commit=False)
+
     # Авторизуем бота
     bot = Bot(token=config.bot_token.get_secret_value(), parse_mode="HTML")
 
     dp = Dispatcher()
 
-    # ПОдключение роутеров
+    # Подключаем хендлеры
     dp.include_router(commands.router)
+    # dp.include_router(inline_mode.router)
+
+    # Устанавливаем команды бота
+    await set_bot_commands(bot)
 
     await dp.start_polling(bot)
 
